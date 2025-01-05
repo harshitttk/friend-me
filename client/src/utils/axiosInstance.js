c71f44f650f0797c5954609f2d50ec921087e18e
Harshit Kumar <harshitsaini61@gmail.com>
2025-01-05 18:54:52 +0530

    Update axiosInstance.js

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an axios instance
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8080/api', // Base URL for the backend API
+  baseURL: 'https://friend-me-one.vercel.app/api', // Base URL for the backend API
   headers: {
     'Content-Type': 'application/json',
   },
