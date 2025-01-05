a12a9e24d4b722c705686209f3179a87771baffb
harshittsaini <harshitsaini61@gmail.com>
2025-01-05 18:22:20 +0530

    code formatted

---
+++ client/src/utils/axiosInstance.js
@@ -0,0 +1,43 @@
+import axios from 'axios';
+
+// Create an axios instance
+const axiosInstance = axios.create({
+  baseURL: 'http://localhost:8080/api', // Base URL for the backend API
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// Interceptor to add the Authorization header to every request
+axiosInstance.interceptors.request.use(
+  (config) => {
+    // Get the token from localStorage
+    const token = localStorage.getItem('token');
+    
+    // If token exists, add it to the Authorization header
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// Interceptor to handle response errors globally
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    // You can add global error handling logic here
+    if (error.response && error.response.status === 401) {
+      // If the response is 401 (Unauthorized), redirect to login
+      localStorage.removeItem('token');
+      window.location.href = '/login';
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
