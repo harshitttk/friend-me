affdf087e9dd5d4c1d671bc19e6c8fe1bb9905e5
Harshit Kumar <harshitsaini61@gmail.com>
2025-01-05 19:21:02 +0530

    Update LoginPage.jsx

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -4,6 +4,9 @@ import AuthForm from "../components/AuthForm";
 const LoginPage = () => {
   return (
     <div className="flex flex-col items-center justify-center">
+      <h3>Test login credentials:</h3>
+      <h4>Username: test</h4>
+      <h4>Password: test123</h4>
       <AuthForm type="login" />
       <p className="mt-4">
         Don't have an account?{" "}
